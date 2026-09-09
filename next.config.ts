e0921264793c8f2f45eaab6f49eaa79e1e2723ed
nt046467-hue <your-email@example.com>
2026-09-09 14:10:43 +0545

    fix(config): add jwks-rsa to serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverExternalPackages: ["firebase-admin"],
+  serverExternalPackages: ["firebase-admin", "jwks-rsa"],
   typescript: {
     ignoreBuildErrors: true,
   },
