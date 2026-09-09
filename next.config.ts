117078cbe25ba68481c4d368f63ef0711ef26c59
nt046467-hue <your-email@example.com>
2026-09-09 14:04:35 +0545

    fix(config): add firebase-admin to serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ["firebase-admin"],
   typescript: {
     ignoreBuildErrors: true,
   },
