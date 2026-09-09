d949b897776509e7ab4828ad859ffd9e72186095
nt046467-hue <your-email@example.com>
2026-09-09 14:15:00 +0545

    fix(config): set serverExternalPackages to firebase-admin

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverExternalPackages: ["firebase-admin", "jwks-rsa"],
+  serverExternalPackages: ["firebase-admin"],
   typescript: {
     ignoreBuildErrors: true,
   },
